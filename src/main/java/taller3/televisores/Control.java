bc03dafee1cd4a94dae5bfee760a825e21959712
Angelsebastian0 <ancuaran@unal.edu.co>
2024-11-18 09:16:40 -0500

    5

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -1,72 +1,91 @@
 package taller3.televisores;
 
-
 public class Control {
-	TV tv;
-	// estado
-	public void turnOn() {
-		tv.turnOn();
-	}
-	public void turnOff() {
-		tv.turnOff();
-	}
-	
-	// cambio de canal
-	public void canalUp() {
-		tv.canalUp();
-	}
-	public void canalDown() {
-		tv.canalDown();
-	}
-	
-	// manejo de volumen
-	public void volumenUp() {
-		tv.volumenUp();
-	}
-	public void volumenDown() {
-		tv.volumenDown();
-	}
-	
-	// set canal y set volumen
-	public int setCanal(int canal) {
-		return tv.canal;
-		
-	}
-	public int setVolumen(int volumen) {
-		return tv.volumen;
-	}
-	
-	//enlazar
-	public void enlazar(TV tv) {
-		this.tv = tv;
-		
-		tv.setControl(this);
-	}
-	public TV getTv() {
-		return tv;
-	}
-    public void setTv(TV tv2) {
-        
+    private TV tv;
+
+    // Encender y apagar
+    public void turnOn() {
+        if (tv != null) {
+            tv.turnOn();
+        }
     }
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+    public void turnOff() {
+        if (tv != null) {
+            tv.turnOff();
+        }
+    }
+
+    // Cambio de canal
+    public void canalUp() {
+        if (tv != null) {
+            tv.canalUp();
+        }
+    }
+
+    public void canalDown() {
+        if (tv != null) {
+            tv.canalDown();
+        }
+    }
+
+    // Manejo de volumen
+    public void volumenUp() {
+        if (tv != null) {
+            tv.volumenUp();
+        }
+    }
+
+    public void volumenDown() {
+        if (tv != null) {
+            tv.volumenDown();
+        }
+    }
+
+    // Cambiar canal y volumen
+    public void setCanal(int canal) {
+        if (tv != null) {
+            tv.setCanal(canal);
+        }
+    }
+
+    public void setVolumen(int volumen) {
+        if (tv != null) {
+            tv.setVolumen(volumen);
+        }
+    }
+
+    // Enlazar control con TV
+    public void enlazar(TV tv) {
+        this.tv = tv;
+        tv.setControl(this);
+    }
+
+    public TV getTv() {
+        return tv;
+    }
+
+    public void setTv(TV tv) {
+        this.tv = tv;
+    }
 }
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
